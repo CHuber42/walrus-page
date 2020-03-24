ae8162501486eff9ef0aeac98ce3c63c407cd383
Allison Mackey <a.mackey693@gmail.com>
2020-03-24 10:15:41 -0700

    adds fadeToggle functions to javascripts.
    co-authored-by: Christopher Huber <CHuber42.work@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".walrus.clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
+  $(".kitten.clickable").click(function() {
+    $("#fade-out-kittens").fadeToggle();
+    $("#fade-in-kittens").fadeToggle(); 
+  });   
 });
+
+    $("PLACEHOLDER").fadeToggle();
+    $("PLACEHOLDER").slideDown();
+    $("PLACEHOLDER").slideUp();
+    $("PLACEHOLDER").slideToggle();
